9866b7330472af22112958cad733cc86cf72ff99
Biner Habib <Binerhabib@gmail.com>
2023-12-11 14:31:58 +0100

    Add project files.

---
+++ Bossfight/GameCharacter.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Bossfight
+{
+    internal class GameCharacter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Stamina { get; set; }
+        private int intStamina { get; set; }       
+
+        public GameCharacter(string name, int health, int strength, int stamina)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            intStamina = stamina;
+        }
+
+        public void fight(GameCharacter opponent)
+        {
+            if (Stamina == 0)
+            {
+                recharge();
+                return;
+            }
+
+            int damageTaken = opponent.Strength;
+            Health -= damageTaken;
+            Console.WriteLine($"Enemy hit for {damageTaken} damage, {Name} now has {Health} health left.");
+            Stamina -= 10;
+
+        }
+
+        public void recharge()
+        {
+            Stamina = intStamina;
+            Console.WriteLine("Character recharged stamina!");
+        }
+    }
+}
---
+++ Bossfight/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bossfight;
+using System.IO.Pipes;
+
+static void main()
+{
+    GameCharacter hero = new GameCharacter("Biner", 100, 40, 10); 
+    GameCharacter boss = new GameCharacter("Beetle Juice", 400, new Random().Next(1,31), 10); 
+
+    while (hero.Health > 0 && boss.Health > 0)
+    {
+        hero.fight(boss);
+        if (boss.Health <= 0)
+        {
+            Console.WriteLine("Biner is the winner");
+            break;
+        }
+
+        boss.fight(hero);
+        if (hero.Health <= 0)
+        {
+            Console.WriteLine("Beetle Juice is the winner");
+            break;
+        }
+    }
+}
+main();
